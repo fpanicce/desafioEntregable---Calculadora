381a36b26c05bd65761f7970af5157976b38ae2b
Franco Panicce <franco_panicce@hotmail.com>
2022-07-10 20:40:38 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Desafio entregable numero 2. Algoritmo Calculadora Simple
 
 const calculadora = (numero1, numero2) =>{
-    const accion = prompt("Indica con el numero que operacion queres hacer: \n1- Sumar \n2- Restar \n3- Multiplicar \n4- Dividir \n0- Salir ")
+    const accion = prompt("Indica con el numero que operacion queres hacer: \n1- Sumar \n2- Restar \n3- Multiplicar \n4- Dividir \n5- Factorial \n6- Serie de Fibonacci \n0- Salir ")
     if(accion == 1){
         const numero1 = Number(prompt("Ingresa el numero 1"))
         const numero2 = Number(prompt("Ingresa el numero 2"))
@@ -22,6 +22,14 @@ const calculadora = (numero1, numero2) =>{
         const numero2 = Number(prompt("Ingresa el numero 2"))
         console.log(`La division de los dos numeros es ${dividir(numero1, numero2)}`)
         alert(`La division de los dos numeros es ${dividir(numero1, numero2)}`)
+    }else if(accion == 5){
+        const numero1 = Number(prompt("Ingresa el numero al que quieres calcularle el factorial"))
+        console.log(`El factorial de ${numero1} es ${factorial(numero1)}`)
+        alert(`El factorial de ${numero1} es ${factorial(numero1)}`)
+    }else if(accion == 6){
+        const rango = Number(prompt("Ingresa el numero al que quieres calcularle la serie de Fibonacci"))
+        console.log(`La serie de fibonacci de ${rango} es ${fibonacci(rango)}`)
+        alert(`La serie de fibonacci de ${rango} es ${fibonacci(rango)}`)
     }else if(accion == 0){
         alert("Que lastima, las matematicas son divertidas")
     }else if(accion == null){
@@ -35,5 +43,22 @@ const sumar = (numero1, numero2) => numero1 + numero2
 const restar = (numero1, numero2) => numero1 - numero2
 const multiplicar = (numero1, numero2) => numero1 * numero2
 const dividir = (numero1, numero2) => numero1 / numero2
+const factorial = (numero1) => {
+    let factorial = 1
+    for(i = 1; i <= numero1; i++){
+        factorial = factorial*i
+    }
+    return factorial
+}
+const fibonacci = (rango) => {
+    let serieFibonacci = []
+    serieFibonacci[0] = 0
+    serieFibonacci[1] = 1
+    for(i = 2; i < rango; i++){
+        serieFibonacci[i] = serieFibonacci[i - 1] + serieFibonacci[i - 2]
+        console.log(serieFibonacci)
+    }
+    return serieFibonacci
+}
 
 calculadora()
